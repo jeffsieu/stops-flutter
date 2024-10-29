fd2d697acd293f62b0410c2be66286c95cf5deaa
Jeff Sieu <jeffsy00@gmail.com>
2024-10-29 15:56:51 +0800

    refactor: extract ReorderableBusStopList widget

--- lib/widgets/bus_stop_overview_list.dart
+++ lib/widgets/bus_stop_overview_list.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart' hide Provider;
 import 'package:provider/provider.dart';
+import 'package:stops_sg/bus_api/models/bus_stop.dart';
 import 'package:stops_sg/database/database.dart';
 import 'package:stops_sg/database/models/user_route.dart';
 import 'package:stops_sg/utils/reorder_status_notification.dart';
-import 'package:stops_sg/widgets/bus_stop_overview_item.dart';
 import 'package:stops_sg/widgets/edit_model.dart';
+import 'package:stops_sg/widgets/reorderable_bus_stop_list.dart';
 
 class BusStopOverviewList extends ConsumerWidget {
   const BusStopOverviewList({super.key, required this.routeId});
@@ -39,130 +40,26 @@ class BusStopOverviewList extends ConsumerWidget {
 
     return Provider<StoredUserRoute>(
       create: (_) => route,
-      child: MediaQuery.removePadding(
-        context: context,
-        removeTop: true,
-        child: ReorderableListView.builder(
-          shrinkWrap: true,
-          buildDefaultDragHandles: false,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: busStops.length,
-          proxyDecorator: (child, index, animation) => child,
+      child: ReorderableBusStopList(
+          busStops: busStops,
+          isEditing: isEditing,
           onReorderStart: (int position) {
             ReorderStatusNotification(true).dispatch(context);
           },
-          onReorder: (
-            int oldIndex,
-            int newIndex,
-          ) async {
-            if (oldIndex < newIndex) {
-              // See: https://api.flutter.dev/flutter/widgets/ReorderCallback.html
-              // removing the item at oldIndex will shorten the list by 1.
-              newIndex -= 1;
-            }
+          onReorderEnd: (position) =>
+              ReorderStatusNotification(false).dispatch(context),
+          onReorder: (int oldIndex, int newIndex) async {
             ReorderStatusNotification(false).dispatch(context);
 
             await ref
                 .read(savedUserRouteProvider(id: routeId).notifier)
                 .moveBusStop(oldIndex, newIndex);
           },
-          itemBuilder: (BuildContext context, int position) {
-            final busStop = busStops[position];
-            final Widget busStopItem = ProxyProvider(
-              update: (context, value, previous) =>
-                  EditModel(isEditing: isEditing),
-              child: BusStopOverviewItem(
-                busStop,
-                key: Key(busStop.code),
-              ),
-            );
-
-            return Stack(
-              key: Key(busStop.hashCode.toString()),
-              alignment: Alignment.centerLeft,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: busStopItem,
-                ),
-                AnimatedOpacity(
-                  duration: const Duration(milliseconds: 600),
-                  opacity: isEditing ? 1.0 : 0.0,
-                  curve: const Interval(0.5, 1),
-                  child: AnimatedSlide(
-                    duration: const Duration(milliseconds: 600),
-                    offset: isEditing ? Offset.zero : const Offset(0, 0.25),
-                    curve: const Interval(0.5, 1, curve: Curves.easeOutCubic),
-                    child: isEditing
-                        ? ReorderableDragStartListener(
-                            index: position,
-                            child: Padding(
-                              padding:
-                                  const EdgeInsetsDirectional.only(start: 32.0),
-                              child: Icon(
-                                Icons.drag_handle_rounded,
-                                color: Theme.of(context).hintColor,
-                              ),
-                            ),
-                          )
-                        : Container(),
-                  ),
-                ),
-                Positioned.fill(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 17.0,
-                        vertical: 9.0), // Offset by 1 to account for outline
-                    child: Material(
-                      type: MaterialType.transparency,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          AnimatedOpacity(
-                            duration: const Duration(milliseconds: 600),
-                            opacity: isEditing ? 1.0 : 0.0,
-                            curve: isEditing
-                                ? const Interval(0.5, 1)
-                                : const Interval(0, 0.25),
-                            child: AnimatedSlide(
-                              duration: const Duration(milliseconds: 600),
-                              offset: isEditing
-                                  ? Offset.zero
-                                  : const Offset(0, 0.25),
-                              curve: isEditing
-                                  ? const Interval(0.5, 1,
-                                      curve: Curves.easeOutCubic)
-                                  : const Interval(0, 0.5,
-                                      curve: Curves.easeOutCubic),
-                              child: Padding(
-                                padding:
-                                    const EdgeInsetsDirectional.only(end: 8.0),
-                                child: IconButton(
-                                  onPressed: () async {
-                                    await ref
-                                        .read(
-                                            savedUserRouteProvider(id: routeId)
-                                                .notifier)
-                                        .removeBusStop(busStop);
-                                  },
-                                  icon: Icon(
-                                    Icons.clear_rounded,
-                                    color: Theme.of(context).hintColor,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
-      ),
+          onBusStopRemoved: (BusStop busStop) async {
+            await ref
+                .read(savedUserRouteProvider(id: routeId).notifier)
+                .removeBusStop(busStop);
+          }),
     );
   }
 }
